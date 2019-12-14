db87545d7a185f8d40d2c1078712f23ee7557561
alvinmukiibi <mukikelly792@gmail.com>
2019-12-14 20:11:41 +0300

    add tailwind and configure it

---
+++ src/components/Welcome.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mx-auto py-5">
+    <button class="bg-blue-500 text-white font-bold rounded-l p-6">{{ name }}</button>
+
+    <div
+      class="bg-green-100 mt-4 border-l-4 border-r-4 border-green-500 text-green-700 px-4 py-3"
+      role="alert"
+    >
+      <p class="font-bold">ALVIN FREELA REAL TIME MESSAGING APP</p>
+      <p class="text-sm">Tech Stack: Laravel, TailwindCSS, VueJS</p>
+      <p class="text-sm">Tools: Webpack, Laravel-Mix, Vue-cli, pusher</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "ALVIN FREELA REAL TIME SYSTEM"
+    };
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+
+<style>
+</style>
